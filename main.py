457bd35eccb874ce452adcb2ed687a0665fbc055
tilaf <tilaf312@mail.ru>
2022-05-29 20:11:34 +0300

    Новые возможности бота

--- main.py
+++ main.py
@@ -4,16 +4,11 @@ import random
 import config
 from MyVkLongPoll import MyVkLongPoll
 
-vk_token = 'fcb715c9650a3a2475974cc92311a806db5164f661c2825ddd5ca195fcdbacd025c8fe4f5bf116d757e93'
-
-vk_session = vk_api.VkApi(token='be24f0ce5f0f311940b38be0c72b3cddc7b2b87e7387170eb5b708ec7714c2c2aecd4ae788b0fed66210c')
+vk_session = vk_api.VkApi(token=config.vk_token)
 longpoll = MyVkLongPoll(vk_session, 213573882)
+abc = 'Привет. как дела, все хорошо'
 
-vadik_id = 150787369
-vage_id = 105746541
-my_id = 65001811
-sipa_id = 151070805
-
+# TODO подумать над структурой
 
 def sender(id, text):
     vk_session.method('messages.send', {'chat_id': id, 'message': text, 'random_id': 0})
@@ -22,26 +17,46 @@ def sender(id, text):
 for event in longpoll.listen():
     print(event)
     ch_id = event.chat_id
-    # если событие - это сообщение
-    if event.type == VkBotEventType.MESSAGE_NEW:
-        # если событие - пришло от конкретного юзера
-        if event.obj.message['from_id'] == vadik_id:
-            # если событие - это фотография
+    # если событие - это сообщение и пришло от конкретного юзера
+    if event.type == VkBotEventType.MESSAGE_NEW and event.obj.message['from_id'] == config.vadik_id:
+        # если событие - это фотография
+        try:
+            if event.obj.message['attachments'][0]['type'] == 'photo':
+                sender(ch_id, random.choice(config.photos_list))
+        except IndexError:
+            print('Сообщение не является фотографией')
+        try:
+            if event.obj.message['attachments'][0]['type'] == 'audio':
+                sender(ch_id, 'Это Мияджи?')
+        except IndexError:
+            print('Сообщение не является аудио')
             try:
-                if event.obj.message['attachments'][0]['type'] == 'photo':
+                if event.obj.message['attachments'][0]['type'] == 'video':
                     sender(ch_id, random.choice(config.photos_list))
             except IndexError:
-                print('Сообщение не является фотографией')
-
-            try:
-                if event.obj.message['attachments'][0]['type'] == 'audio':
-                    sender(ch_id, 'Это Мияджи?')
-            except IndexError:
-                print('Сообщение не является аудио')
-            try:
-                if event.obj.message['attachments'][0]['type'] == 'wall':
-                    sender(ch_id, random.choice(config.photos_list))
-            except IndexError:
-                print('Сообщение не является репостом')
-        else:
-            print('Сообщение не от Вадика')
+                print('Сообщение не является видео')
+        try:
+            if event.obj.message['attachments'][0]['type'] == 'wall':
+                sender(ch_id, random.choice(config.photos_list))
+        except IndexError:
+            print('Сообщение не является репостом')
+    elif event.type == VkBotEventType.MESSAGE_NEW:
+        # TODO подумать как оптимизировать
+        text_list = event.obj.message['text'].lower().replace(',', '')
+        text_list = text_list.replace('.', '')
+        text_list = text_list.replace('!', '')
+        text_list = text_list.replace('?', '')
+        text_list = text_list.replace('"', '')
+        text_list = text_list.split()
+        coincidence_weather = None
+        coincidence_bot = None
+        for word in text_list:
+            if word in config.list_name_weather:
+                coincidence_weather = True
+                break
+        for word in text_list:
+            if word in config.list_name_bot:
+                coincidence_bot = True
+                break
+        if coincidence_bot and coincidence_weather:
+            sender(ch_id, 'запросили погоду')
