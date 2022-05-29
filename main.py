3cf27db2794ade9c6d968aed73e2d2fba1a67ee6
tilaf <tilaf312@mail.ru>
2022-05-30 00:57:11 +0300

    Доработки

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import vk_api, json
+import vk_api
+import weather_handler
 from vk_api.bot_longpoll import VkBotEventType
 import random
 import config
@@ -6,7 +7,7 @@ from MyVkLongPoll import MyVkLongPoll
 
 vk_session = vk_api.VkApi(token=config.vk_token)
 longpoll = MyVkLongPoll(vk_session, 213573882)
-abc = 'Привет. как дела, все хорошо'
+
 
 # TODO подумать над структурой
 
@@ -14,11 +15,18 @@ def sender(id, text):
     vk_session.method('messages.send', {'chat_id': id, 'message': text, 'random_id': 0})
 
 
+def get_weather(city):
+    latitude, longitude = weather_handler.geo_pos(city)
+    yandex_weather_x = weather_handler.yandex_weather(latitude, longitude, weather_handler.token)
+    return weather_handler.print_yandex_weather(yandex_weather_x)
+
+
 for event in longpoll.listen():
     print(event)
     ch_id = event.chat_id
-    # если событие - это сообщение и пришло от конкретного юзера
-    if event.type == VkBotEventType.MESSAGE_NEW and event.obj.message['from_id'] == config.vadik_id:
+    # если событие - это сообщение и пришло от Вадика и не является обычным текстовым сообщением
+    if event.type == VkBotEventType.MESSAGE_NEW and event.obj.message['from_id'] == config.my_id and not len(
+            event.obj.message['attachments']) == 0:
         # если событие - это фотография
         try:
             if event.obj.message['attachments'][0]['type'] == 'photo':
@@ -30,11 +38,11 @@ for event in longpoll.listen():
                 sender(ch_id, 'Это Мияджи?')
         except IndexError:
             print('Сообщение не является аудио')
-            try:
-                if event.obj.message['attachments'][0]['type'] == 'video':
-                    sender(ch_id, random.choice(config.photos_list))
-            except IndexError:
-                print('Сообщение не является видео')
+        try:
+            if event.obj.message['attachments'][0]['type'] == 'video':
+                sender(ch_id, random.choice(config.photos_list))
+        except IndexError:
+            print('Сообщение не является видео')
         try:
             if event.obj.message['attachments'][0]['type'] == 'wall':
                 sender(ch_id, random.choice(config.photos_list))
@@ -59,4 +67,4 @@ for event in longpoll.listen():
                 coincidence_bot = True
                 break
         if coincidence_bot and coincidence_weather:
-            sender(ch_id, 'запросили погоду')
+                sender(ch_id, get_weather('Ярославль'))
---
+++ weather_handler.py
@@ -0,0 +1,59 @@
+import json
+import config
+import requests as req
+from geopy import geocoders
+
+token = config.yandex_token
+
+
+def geo_pos(city: str):
+    geolocator = geocoders.Nominatim(user_agent="vkbot")
+    latitude = str(geolocator.geocode(city).latitude)
+    longitude = str(geolocator.geocode(city).longitude)
+    return latitude, longitude
+
+
+def yandex_weather(latitude, longitude, token_yandex):
+    url_yandex = f'https://api.weather.yandex.ru/v2/informers?lat={latitude}&lon={longitude}&[lang=ru_RU]'
+    yandex_req = req.get(url_yandex, headers={'X-Yandex-API-Key': token_yandex}, verify=False)
+    conditions = {'clear': 'ясно', 'partly-cloudy': 'малооблачно', 'cloudy': 'облачно с прояснениями',
+                  'overcast': 'пасмурно', 'drizzle': 'морось', 'light-rain': 'небольшой дождь',
+                  'rain': 'дождь', 'moderate-rain': 'умеренно сильный', 'heavy-rain': 'сильный дождь',
+                  'continuous-heavy-rain': 'длительный сильный дождь', 'showers': 'ливень',
+                  'wet-snow': 'дождь со снегом', 'light-snow': 'небольшой снег', 'snow': 'снег',
+                  'snow-showers': 'снегопад', 'hail': 'град', 'thunderstorm': 'гроза',
+                  'thunderstorm-with-rain': 'дождь с грозой', 'thunderstorm-with-hail': 'гроза с градом'
+                  }
+    wind_dir = {'nw': 'северо-западное', 'n': 'северное', 'ne': 'северо-восточное', 'e': 'восточное',
+                'se': 'юго-восточное', 's': 'южное', 'sw': 'юго-западное', 'w': 'западное', 'с': 'штиль'}
+
+    yandex_json = json.loads(yandex_req.text)
+    yandex_json['fact']['condition'] = conditions[yandex_json['fact']['condition']]
+    yandex_json['fact']['wind_dir'] = wind_dir[yandex_json['fact']['wind_dir']]
+    for parts in yandex_json['forecast']['parts']:
+        parts['condition'] = conditions[parts['condition']]
+        parts['wind_dir'] = wind_dir[parts['wind_dir']]
+
+    pogoda = dict()
+    params = ['condition', 'wind_dir', 'pressure_mm', 'humidity']
+    for parts in yandex_json['forecast']['parts']:
+        pogoda[parts['part_name']] = dict()
+        pogoda[parts['part_name']]['temp'] = parts['temp_avg']
+        for param in params:
+            pogoda[parts['part_name']][param] = parts[param]
+
+    pogoda['fact'] = dict()
+    pogoda['fact']['temp'] = yandex_json['fact']['temp']
+    for param in params:
+        pogoda['fact'][param] = yandex_json['fact'][param]
+
+    pogoda['link'] = yandex_json['info']['url']
+    return pogoda
+
+
+# TODO сделать цикл правильно
+def print_yandex_weather(dict_weather_yandex):
+    weather_list = f'Температура утром - {dict_weather_yandex["morning"]["temp"]}, осадки - {dict_weather_yandex["morning"]["condition"]}' \
+                   f'\nТемпература днем - {dict_weather_yandex["day"]["temp"]}, осадки - {dict_weather_yandex["day"]["condition"]}' \
+                   f'\nТемпература сейчас - {dict_weather_yandex["fact"]["temp"]}, осадки - {dict_weather_yandex["fact"]["condition"]}'
+    return weather_list
