e3808f873636e332d9ceb99d29edd61ec1e2ab35
tilaf <tilaf312@mail.ru>
2022-05-29 13:45:34 +0300

    Добавил переопредление класса, чтобы Бот не падал по таймауту. Добавил событие - репост в беседу.

---
+++ MyVkLongPoll.py
@@ -0,0 +1,12 @@
+from vk_api.bot_longpoll import VkBotLongPoll
+
+
+class MyVkLongPoll(VkBotLongPoll):
+    def listen(self):
+        while True:
+            try:
+                for event in self.check():
+                    yield event
+            except Exception as e:
+                print('Таймаут бота', e)
+                ...
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import vk_api, json
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.bot_longpoll import VkBotEventType
 import random
 import config
+from MyVkLongPoll import MyVkLongPoll
 
 vk_token = 'fcb715c9650a3a2475974cc92311a806db5164f661c2825ddd5ca195fcdbacd025c8fe4f5bf116d757e93'
 
 vk_session = vk_api.VkApi(token='be24f0ce5f0f311940b38be0c72b3cddc7b2b87e7387170eb5b708ec7714c2c2aecd4ae788b0fed66210c')
-longpoll = VkBotLongPoll(vk_session, 213573882)
+longpoll = MyVkLongPoll(vk_session, 213573882)
 
 vadik_id = 150787369
 vage_id = 105746541
@@ -24,7 +25,7 @@ for event in longpoll.listen():
     # если событие - это сообщение
     if event.type == VkBotEventType.MESSAGE_NEW:
         # если событие - пришло от конкретного юзера
-        if event.obj.message['from_id'] == vadik_id:
+        if event.obj.message['from_id'] == my_id:
             # если событие - это фотография
             try:
                 if event.obj.message['attachments'][0]['type'] == 'photo':
@@ -37,5 +38,10 @@ for event in longpoll.listen():
                     sender(ch_id, 'Это Мияджи?')
             except IndexError:
                 print('Сообщение не является аудио')
+            try:
+                if event.obj.message['attachments'][0]['type'] == 'wall':
+                    sender(ch_id, random.choice(config.photos_list))
+            except IndexError:
+                print('Сообщение не является репостом')
         else:
             print('Сообщение не от Вадика')
